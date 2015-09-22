92a644760e5b7fc557b1bb291597766da465932d
Victor Valencia <vicvalen@gmail.com>
2015-09-22 13:20:03 -0700

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ActiveRecord::Base
 	validates :description, presence: true
+
+	belongs_to :user
+	validates :user_id, presence: true
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :pins
   get 'about' => 'pages#about'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
