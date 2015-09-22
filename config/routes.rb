38542626b54e2f95f579663f728376e072b33b06
Victor Valencia <vicvalen@gmail.com>
2015-09-22 16:52:01 -0700

    Reroute root pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'pins#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
